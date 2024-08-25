c51b978085154d7313c874c1de82add69f85b3a3
w <w.maas@proton.me>
2024-08-25 12:14:07 +0200

    chore: Add backend TypeScript configuration and package.json updates

---
+++ backend/dist/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+const PORT = 8080;
+const rootDir = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+app.use(express.json());
+app.listen(PORT, async () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+// Serve the root route with coffeeai.html
+app.get("/", (req, res) => {
+    res.sendFile(path.join(rootDir, "../../code2024", "coffeeai.html"));
+});
+// Serve static files
+app.use(express.static(path.join(rootDir, "../../code2024")));
+//# sourceMappingURL=index.js.map
